de090313f77a7d0fe8f95b64f0989683f7028fd0
Imelda Santos <isantos1206@gmail.com>
2015-04-18 05:14:00 -0500

    Committed by Udacity

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+              rename: false,
+              width: 400,
+              quality: 60
+            },{
+              name: 'large',
+              width: 800,
+              quality: 50
+            },{
+              name: "large",
+              width: 1200,
+              suffix: "_x2",
+              quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,webp}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: 'images_src',
+          src: 'fixed/*.{gif,jpg,png,webp}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
